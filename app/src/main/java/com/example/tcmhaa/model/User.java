fa07d617e2ecd91cadb9499fa274e6984c79f0c1
Yun <wxyun19@gmail.com>
2025-08-17 23:31:45 +0800

    登入model

---
+++ app/src/main/java/com/example/tcmhaa/model/User.java
@@ -0,0 +1,12 @@
+package com.example.tcmhaa.model;
+
+public class User {
+    public int user_id;
+    public String name;
+    public String email;
+    // Getter
+    public int getUserId() { return user_id; }
+
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+}
